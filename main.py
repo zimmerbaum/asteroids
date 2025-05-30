cb977fb99b673d04fdd7fa4a73e03d210f218bc0
zimmerbaum <luzi79.lz@gmail.com>
2025-05-30 23:53:16 +0200

    CH2 L3

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ from constants import *
 def main():
     pygame.init()
 
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -17,6 +20,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
